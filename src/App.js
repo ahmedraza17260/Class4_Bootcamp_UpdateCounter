da1518f1a664d71bdd3f2018f94a54de2adbbce5
ahmedraza17260 <ahmedraza17260@gmail.com>
2020-06-12 22:33:44 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import logo from './logo.svg';
 import "./App.css";
 import { Message } from "./Message.js";
 
@@ -7,24 +6,7 @@ function App() {
     let [count, setCount] = useState(1);
     let [isDay, setDay] = useState(true);
 
-    return (
-        // <div className="App">
-        //   <header className="App-header">
-        //     <img src={logo} className="App-logo" alt="logo" />
-        //     <p>
-        //       Edit <code>src/App.js</code> and save to reload.
-        //     </p>
-        //     <a
-        //       className="App-link"
-        //       href="https://reactjs.org"
-        //       target="_blank"
-        //       rel="noopener noreferrer"
-        //     >
-        //       Learn React
-        //     </a>
-        //   </header>
-        // </div>
-        <
+    return ( <
         div className = { `box ${isDay ? "daylight" : ""}` } >
         <
         h1 > Good { isDay ? "Morning" : "Night" } < /h1> <Message counter={count} / >
@@ -32,9 +14,9 @@ function App() {
         br / >
         <
         button onClick = {
-            () => setCount(++count) } > Updated Counter < /button> <
+            () => setCount(++count) } > Updated Counter < /button>{" "} <
         button onClick = {
-            () => setDay(!isDay) } > Update Day < /button> <
+            () => setDay(!isDay) } > Update Day < /button>{" "} <
         /div>
     );
 }
